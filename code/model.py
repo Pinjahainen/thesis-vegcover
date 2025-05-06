558f38e34b9e8315f531310f099cee719b1b600e
Pinjahainen <77828005+Pinjahainen@users.noreply.github.com>
2025-05-06 10:40:27 +0300

    Update model.py

--- code/model.py
+++ code/model.py
@@ -23,8 +23,8 @@ import seaborn as sns
 from sklearn.metrics import accuracy_score, confusion_matrix
 
 year = int(input('Enter year:'))
-all = pd.DataFrame(pd.read_excel(r"file.xlsx"))
-obs_orig = pd.DataFrame(pd.read_excel(r"file.xlsx"))
+all = pd.DataFrame(pd.read_excel(r"file.xlsx")) # the data saved in get_data.py
+obs_orig = pd.DataFrame(pd.read_excel(r"file.xlsx")) # in situ data
 obs_orig=obs_orig.loc[obs_orig['vuosi']==year]
 print(obs_orig)
 
@@ -57,18 +57,18 @@ counts = np.zeros((8,10))
 
 for i in range(8):
     for j in range(10):
-        newdata = df.loc[df['S0']==i]
-        newdata = newdata.loc[newdata['S1']==j]
+        newdata = df.loc[df['Y0']==i]
+        newdata = newdata.loc[newdata['Y1']==j]
         n = len(newdata['field_id'])
         counts[i][j] = n
 
 
-countsdf = pd.DataFrame(counts, columns=(['S1=0', 'S1=1', 'S1=2', 'S1=3', 'S1=4', 'S1=5', 'S1=6', 'S1=7', 'S1=8', 'S1=9']), index=(['S0=0', 'S0=1', 'S0=2', 'S0=3', 'S0=4', 'S0=5', 'S0=6', 'S0=7']))
+countsdf = pd.DataFrame(counts, columns=(['Y1=0', 'Y1=1', 'Y1=2', 'Y1=3', 'Y1=4', 'Y1=5', 'Y1=6', 'Y1=7', 'Y1=8', 'Y1=9']), index=(['Y0=0', 'Y0=1', 'Y0=2', 'Y0=3', 'Y0=4', 'Y0=5', 'Y0=6', 'Y0=7']))
 
 print(countsdf)
 
 
-winter_final = pd.DataFrame(pd.read_excel(r'file.xlsx'))    
+winter_final = pd.DataFrame(pd.read_excel(r'file.xlsx'))    # The winter conditional probabilities presented in "probabilities" file
 w0 = winter_final.loc[0, :].values.flatten().tolist()
 w1 = winter_final.loc[1, :].values.flatten().tolist()
 w2 = winter_final.loc[2, :].values.flatten().tolist()
@@ -78,36 +78,36 @@ w5 = winter_final.loc[5, :].values.flatten().tolist()
 w6 = winter_final.loc[6, :].values.flatten().tolist()
 
 
-autumn_final = pd.DataFrame(pd.read_excel(r'file.xlsx'))
+autumn_final = pd.DataFrame(pd.read_excel(r'file.xlsx')) # The autumn conditional probabilities presented in "probabilities" file
 
 A0 = autumn_final.loc[0, :].values.flatten().tolist()
 A1 = autumn_final.loc[1, :].values.flatten().tolist()
 A2 = autumn_final.loc[2, :].values.flatten().tolist()
 
-frq_S0, frq_S1 = fun.freq(all_orig)
+frq_Y0, frq_Y1 = fun.freq(all_orig)
 
 # Defining Bayesian Structure
-model = BayesianNetwork([('S1', 'S1_real'), ('WinterKill', 'S1_real'), ('S0', 'Winter'), ('S1_real', 'Winter'), ('Winter', 'Autumn'), ('Report', 'Autumn')])
-sr = 0.979 # success rate of 
+model = BayesianNetwork([('Y1', 'Y1_real'), ('WinterKill', 'Y1_real'), ('Y0', 'Winter'), ('Y1_real', 'Winter'), ('Winter', 'Autumn'), ('Report', 'Autumn')])
+sr = 0.979 # success rate of winter crops. Calculated from statistics. Year 2022-2023
 # Defining the CPDs:
 cpd_WK = TabularCPD('WinterKill', 2, [[1-sr], [sr]])
-cpd_S0 = TabularCPD('S0', len(frq_S0), frq_S0) 
-cpd_S1 = TabularCPD('S1', len(frq_S1), frq_S1)
+cpd_Y0 = TabularCPD('Y0', len(frq_Y0), frq_Y0)  # prior distribution of year 0 main crops.
+cpd_Y1 = TabularCPD('Y1', len(frq_Y1), frq_Y1) # prior distribution of year 1 main crops.
 cpd_report = TabularCPD('Report', 2, [[0.5],[0.5]])
-cpd_S1R = TabularCPD('S1_real', 10, [
+cpd_Y1R = TabularCPD('Y1_real', 10, [
                         ####
                         ####
-                            [1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], # syysvilja
-                            [0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], # kevätkylvöinen yksivuotinen
-                            [0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0], # aluskasvi
-                            [0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0], # muu kasvipeite
-                            [0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0], # nurmi
-                            [0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0], # pysyvä kasvipeite
-                            [0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0], # kumina
-                            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0], # sänkikesanto
-                            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0], # kevätkylvöinen ei suorakylvettävä
-                            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1]], # syysvilja tai kevätkylvöinen
-                  evidence=['S1','WinterKill'], evidence_card=[len(frq_S1),2])
+                            [0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], # Winter crop
+                            [1,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], # Spring-sown annual
+                            [0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0], # Companion crop
+                            [0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0], # Other vegetation
+                            [0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0], # Grass
+                            [0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0], # Permanent vegetation
+                            [0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0], # Stubble
+                            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0], # Spring-sown no direct seeding
+                            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0], # Winter or spring crop
+                            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1]], # Unknown
+                  evidence=['Y1','WinterKill'], evidence_card=[len(frq_Y1),2])
 cpd_winter = TabularCPD('Winter', 7,
                             [w0, # Winter = 0 "Ploughed or Tillaged"
                             w1, # 1 "Winter crop"
@@ -116,7 +116,7 @@ cpd_winter = TabularCPD('Winter', 7,
                             w4, # 4 "Stubble from other crop"
                             w5, # 5 "Stubble and undersown plant"
                             w6], # 6 other
-                            evidence=['S0', 'S1_real'], evidence_card=[len(frq_S0), 10])
+                            evidence=['Y0', 'Y1_real'], evidence_card=[len(frq_Y0), 10])
 cpd_autumn = TabularCPD('Autumn', 3,
                         [A0, A1, A2],
                         evidence = ['Winter', 'Report'], evidence_card=[7,2])
@@ -124,14 +124,14 @@ cpd_autumn = TabularCPD('Autumn', 3,
 
  
 # Associating the CPDs with the network structure.
-model.add_cpds(cpd_S0, cpd_S1, cpd_WK, cpd_S1R, cpd_winter, cpd_autumn, cpd_report)
+model.add_cpds(cpd_Y0, cpd_Y1, cpd_WK, cpd_Y1R, cpd_winter, cpd_autumn, cpd_report)
 
 model.check_model()
 
 from pgmpy.inference import VariableElimination
  
 infer = VariableElimination(model)
-posterior_p = infer.query(variables=['Winter'], evidence={'S0': 0, 'S1': 4, 'Autumn':0, 'Report':1})
+posterior_p = infer.query(variables=['Winter'], evidence={'Y0': 0, 'Y1': 4, 'Autumn':0, 'Report':1})
 print(posterior_p)
 
 
@@ -144,22 +144,22 @@ df = df.astype({'onraportti': 'int64'})
 
 
 
-n = len(df['S0'])-1
+n = len(df['Y0'])-1
 print(n)
 
-nS0 = 9
-nS1 = 11
+nY0 = 9
+nY1 = 11
 nA = 3
 nR = 2
-s0 = df['S0']
-s1 = df['S1']
+Y0 = df['Y0']
+Y1 = df['Y1']
 autumn = df['autumn']
 report = df['onraportti']
 
 
-choises = list(range(nS0*nS1*nA*nR))
+choises = list(range(nY0*nY1*nA*nR))
 
-conditions = [(s0 == a) & (s1 == b) & (autumn == c) & (report == d) for a in range(nS0) for b in range(nS1) for c in range(nA) for d in range(nR)]
+conditions = [(Y0 == a) & (Y1 == b) & (autumn == c) & (report == d) for a in range(nY0) for b in range(nY1) for c in range(nA) for d in range(nR)]
 
 df['group'] = np.select(conditions, choises, 100000)
 
@@ -172,18 +172,18 @@ for i in range(n):
     #if i == 1165:
     #    i=1166
     group = df.loc[df['group']==i]
-    if len(group['S0'])==0:
+    if len(group['Y0'])==0:
         pass
     else:
-        a = group['S0'].iloc[-1]
-        b = group['S1'].iloc[-1]
+        a = group['Y0'].iloc[-1]
+        b = group['Y1'].iloc[-1]
         c = group['autumn'].iloc[-1]
         d = group['onraportti'].iloc[-1]
 
         infer = VariableElimination(model)
-        posterior = infer.query(variables=['Winter'], evidence={'S0': a, 'S1': b, 'Autumn': c, 'Report': d})
+        posterior = infer.query(variables=['Winter'], evidence={'Y0': a, 'Y1': b, 'Autumn': c, 'Report': d})
         elements = range(7)
-        # S1 = 7,8 causes NaN, transform them to uniform distribution
+        # Y1 = 7,8 causes NaN, transform them to uniform distribution
         probabilities = pd.DataFrame(posterior.values)
         probabilities = probabilities.fillna(1/7)
     
@@ -221,10 +221,10 @@ fun.cm_analysis(actual22, predicted22, r'file.jpg', [0,1,2,3,4,5], ymap=None, fi
 
 
 traindf = all_train.loc[all_train['truth'].isin([0,1,2,3,4,5,6])]
-traindf = traindf[['S0', 'S1', 'autumn', 'onraportti', 'truth']]
+traindf = traindf[['Y0', 'Y1', 'autumn', 'onraportti', 'truth']]
 traindf = traindf.fillna(0)
 traindf['WinterKill'] = 0
-traindf['S1_real'] = traindf['S1']
+traindf['Y1_real'] = traindf['Y1']
 
 traindf.rename(columns = {'onraportti':'Report', 'autumn':'Autumn', 'truth': 'Winter'}, inplace = True)
 traindf
@@ -237,19 +237,19 @@ df = unknown.reset_index()
 df['onraportti'] = df['onraportti'].fillna(0)
 df = df.astype({'onraportti': 'int64'})
 
-n = len(df['S0'])-1
+n = len(df['Y0'])-1
 print(n)
 
 nA = 3
 nR = 2
-s0 = df['S0']
-s1 = df['S1']
+Y0 = df['Y0']
+Y1 = df['Y1']
 autumn = df['autumn']
 report = df['onraportti']
 
-choises = list(range(nS0*nS1*nA*nR))
+choises = list(range(nY0*nY1*nA*nR))
 
-conditions = [(s0 == a) & (s1 == b) & (autumn == c) & (report == d) for a in range(nS0) for b in range(nS1) for c in range(nA) for d in range(nR)]
+conditions = [(Y0 == a) & (Y1 == b) & (autumn == c) & (report == d) for a in range(nY0) for b in range(nY1) for c in range(nA) for d in range(nR)]
 
 df['group'] = np.select(conditions, choises, 100000)
 
@@ -263,18 +263,18 @@ for i in range(n):
     #if i == 1165:
     #    i=1166
     group = df.loc[df['group']==i]
-    if len(group['S0'])==0:
+    if len(group['Y0'])==0:
         pass
     else:
-        a = group['S0'].iloc[-1]
-        b = group['S1'].iloc[-1]
+        a = group['Y0'].iloc[-1]
+        b = group['Y1'].iloc[-1]
         c = group['autumn'].iloc[-1]
         d = group['onraportti'].iloc[-1]
 
         infer = VariableElimination(model)
-        posterior = infer.query(variables=['Winter'], evidence={'S0': a, 'S1': b, 'Autumn': c, 'Report': d})
+        posterior = infer.query(variables=['Winter'], evidence={'Y0': a, 'Y1': b, 'Autumn': c, 'Report': d})
         elements = range(7)
-        # S1 = 7,8 causes NaN, transform them to uniform distribution
+        # Y1 = 7,8 causes NaN, transform them to uniform distribution
         probabilities = pd.DataFrame(posterior.values)
         probabilities = probabilities.fillna(1/7)
     
